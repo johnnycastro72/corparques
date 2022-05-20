f5d38b21e1b907934659dff1f9d08ab5ad19c0c5
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-20 12:10:42 -0500

    Initial commit

---
+++ src/main/java/co/com/sofka/corparques/CorparquesApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.corparques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorparquesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorparquesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/sofka/corparques/CorparquesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.corparques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorparquesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
